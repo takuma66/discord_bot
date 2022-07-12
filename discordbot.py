59661d8fdc5632ae426a9e3bcf7998a31c065140
takuma66 <kanza414@gmail.com>
2022-07-12 22:26:17 +0900

    commit

---
+++ discordbot.py
@@ -0,0 +1,55 @@
+import discord
+import random
+
+TOKEN = 'OTk2NDAzNzkwMzE4MTQ5NzEy.Gp6YKa.1-ImE1QEmdqCXra6VaKlLT9hwG9rYnJBQe-jps' # TOKENを貼り付け
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+random_maps = [
+    "アセント",
+    "スプリット",
+    "ヘイブン",
+    "バインド",
+    "アイスボックス",
+    "ブリーズ",
+    "フラクチャー",
+    "パール"
+]
+
+cointoss = [
+    "表",
+    "裏"
+]
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+
+    if message.content == "/map":
+        content = random.choice(random_maps)
+        await message.channel.send(content)
+    elif message.content == "/coin":
+        content = random.choice(cointoss)
+        await message.channel.send(content)
+    elif message.content == "/team":
+        channel = client.get_channel(727557374415601737)
+        members = channel.members
+        random.shuffle(members)
+        harf = int(len(members)/2)
+        team = members[:harf]
+        for mem in team:
+            await message.channel.send(mem.display_name)
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
